d28924e70dca6cc5c1e0418c76280c3c5e72db2d
TCFII <168501077+TCFII@users.noreply.github.com>
2024-07-15 23:20:31 +0000

    delet kinda works

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -2,7 +2,7 @@ import React, {useState } from 'react';
 
 export function ToDoList ({ }) {
 
-const [todos,setTodos]=useState(["Go to work","Drive the car","Go to Publix"]);
+const [todos,setTodos]=useState([]);
 const [task,setTask]=useState("");
 
 const addTask = () => {
@@ -22,35 +22,34 @@ const handleEnterKey = (ev) => {
     }
 };
 const handleDelete = (index) => {
-    const newTodos = todos.filter((todos,i) =>i !== index);
+    const newTodos = todos.filter((todos) => todos.index !== index);
     setTodos(newTodos);
 };
 
 return (
 <div className="w-3/4 flex flex-col items-center border-2 rounded-lg border-black">
-    <h1 className="text-xl mt-5">To Do List</h1>
+    <h1 className="text-xl mt-4 mb-4">To Do List</h1>
     <div>
-        <input  className="w-full border-2 rounded-lg border-black" 
+        <input  className="w-full border-2 rounded-lg border-black mb-4" 
                 placeholder="Enter new task!" 
                 value={task}
                 onChange={handleChangeTask}
                 onKeyDown={handleEnterKey}
         />
     </div>
-    <div className="w-1/2 border-2 rounded-lg border-gray">
+    <div className="w-1/2 border-2 rounded-lg border-gray mb-4">
         <ul>
             {todos.map((todos,index) => (
                 <li className="flex justify-between mr-2" key={index}>{todos} {index}
-                <button onClick={() => handleDelete (todos.id)} class="ml-auto text-[10px] text-white hover:text-red-500 rounded">
+                <button onClick={() => handleDelete (todos.index)} class="ml-auto text-[10px] text-white hover:text-red-500 rounded">
                     X
                 </button>
                 </li>
             ))}
         </ul>
     </div>
-    <div className="w-1/2 border-2 rounded-lg border-gray">
-       {todos.length}
-            <p>To do items left:&nbsp;{todos.length}</p>
+    <div className="w-1/2 border-2 rounded-lg border-transparent mb-4">
+       <p>To do items left:&nbsp;{todos.length}</p>
     </div>
 
 
